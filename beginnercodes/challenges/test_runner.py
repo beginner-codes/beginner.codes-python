6ab9a9f6c41928fe1f289233b0f9af5828f70de7
Zech Zimmerman <hi@zech.codes>
2022-08-05 20:34:40 -0400

    Protect test running functions from being called to prevent cheating

--- beginnercodes/challenges/test_runner.py
+++ beginnercodes/challenges/test_runner.py
@@ -24,57 +24,55 @@ class Result:
 
 
 def test(challenge: int, solution_func):
+    ### These functions must be protected to ensure no CHEATING happens. ###
+    def _get_tests(challenge: int) -> list[dict]:
+        response = requests.get(
+            f"https://raw.githubusercontent.com/beginner-codes/challenges/main/weekday/test_cases_{challenge}.json"
+        )
+        if response.status_code == 404:
+            raise ChallengeNotFound(f"Challenge {challenge} was not found.")
+
+        return response.json()
+
+    def _run_tests(tests: list[dict], solution_func) -> list[Result]:
+        results = []
+        for index, test_case in enumerate(tests, start=1):
+            result = Result(index, test_case["return"])
+            try:
+                result.got = solution_func(*test_case["args"])
+            except Exception as exp:
+                result.status = StatusEnum.EXCEPTION
+                result.got = exp
+            else:
+                if result.got != test_case["return"]:
+                    result.status = StatusEnum.FAILED
+
+            results.append(result)
+
+        return results
+
+    def _show_results(challenge: int, results: list[Result], total_tests: int):
+        failures = 0
+        for result in results:
+            if result.status == StatusEnum.FAILED:
+                print(
+                    f"Test {result.index} failed:  Expected {result.expected}, got {result.got}."
+                )
+                failures += 1
+
+            elif result.status == StatusEnum.EXCEPTION:
+                print(f"Test {result.index} failed:  {result.got!r}")
+                failures += 1
+
+        if failures:
+            print()
+
+        print(f"---- Challenge {challenge} Results ----")
+        print(f"{total_tests - failures} passed, {failures} failed")
+
+        if not failures:
+            print("\n**** Great job!!! ****")
+
     tests = _get_tests(challenge)
     results = _run_tests(tests, solution_func)
     _show_results(challenge, results, len(tests))
-
-
-def _get_tests(challenge: int) -> list[dict]:
-    response = requests.get(
-        f"https://raw.githubusercontent.com/beginner-codes/challenges/main/weekday/test_cases_{challenge}.json"
-    )
-    if response.status_code == 404:
-        raise ChallengeNotFound(f"Challenge {challenge} was not found.")
-
-    return response.json()
-
-
-def _run_tests(tests: list[dict], solution_func) -> list[Result]:
-    results = []
-    for index, test_case in enumerate(tests, start=1):
-        result = Result(index, test_case["return"])
-        try:
-            result.got = solution_func(*test_case["args"])
-        except Exception as exp:
-            result.status = StatusEnum.EXCEPTION
-            result.got = exp
-        else:
-            if result.got != test_case["return"]:
-                result.status = StatusEnum.FAILED
-
-        results.append(result)
-
-    return results
-
-
-def _show_results(challenge: int, results: list[Result], total_tests: int):
-    failures = 0
-    for result in results:
-        if result.status == StatusEnum.FAILED:
-            print(
-                f"Test {result.index} failed:  Expected {result.expected}, got {result.got}."
-            )
-            failures += 1
-
-        elif result.status == StatusEnum.EXCEPTION:
-            print(f"Test {result.index} failed:  {result.got!r}")
-            failures += 1
-
-    if failures:
-        print()
-
-    print(f"---- Challenge {challenge} Results ----")
-    print(f"{total_tests - failures} passed, {failures} failed")
-
-    if not failures:
-        print("\n**** Great job!!! ****")
