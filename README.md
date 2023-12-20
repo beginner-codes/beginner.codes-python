121579217b9c95d4c7de907bf3ac63bca9ff26cf
Rob <mrrobinhood5@gmail.com>
2023-12-20 08:52:06 -0600

    Visual enhancements to TestRunner (#1)
    * - Option to display description and examples
    - Used tables to display result
    - added some colors
    
    * - bumped version in toml file
    - passing a function is now optional
    - passing incorrect arguments prints USAGE
    - _fetch_data only returns Response (No Union)
    - fixed usages of .replace
    - cleared up _get_info instead of terniary ifs
    
    * - fixed a minor bug with USAge

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This is the official Python Package for the Beginner.Codes Discord server.
 - Install the package: `pip install beginner.codes`
 - Import the test runner: `from beginnercodes.challenges import test`
 - Run the tests, passing in the challenge number and your solution function: `test(458, n_differences)`
+- Optionally, you can add two options `description=True` and `examples=True` to print out the challenge information without leaving the terminal
 ```python
 from beginnercodes.challenges import test
 
@@ -15,6 +16,9 @@ def n_differences(nums: list[int]) -> int:
 
 
 test(458, n_differences)
+test(458, n_differences, description=True)
+test(458, n_differences, examples=True)
+test(458, n_differences, True, True) # for both
 ```
 This will handle downloading the necessary challenge test cases and will run them against your code. It will show you which tests failed, what went wrong, and how many tests succeeded.
 
--- beginnercodes/challenges/test_runner.py
+++ beginnercodes/challenges/test_runner.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from enum import Enum, auto
-from typing import Any
+from typing import Any, Optional
+from requests import Response
+from rich.markdown import Markdown
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
+from rich import box
 import requests
 
 
@@ -23,16 +29,57 @@ class Result:
     status: StatusEnum = field(default=StatusEnum.SUCCESS)
 
 
-def test(challenge: int, solution_func):
-    ### These functions must be protected to ensure no CHEATING happens. ###
-    def _get_tests(challenge: int) -> list[dict]:
-        response = requests.get(
-            f"https://raw.githubusercontent.com/beginner-codes/challenges/main/weekday/test_cases_{challenge}.json"
-        )
-        if response.status_code == 404:
-            raise ChallengeNotFound(f"Challenge {challenge} was not found.")
+USAGE = """# **USAGE** 
+```py
+test(challenge: int, your_function: func, description: bool, examples: bool) 
+---
+test(458, your_function)   # runs all tests cases on your function  
+test(458, your_function, description=True)  # adds the test description in the terminal
+test(458, your_function, examples=True)  # adds test examples in the terminal
+test(458, your_function, True, True)  # adds both description and examples
+test(458, description=True)  # prints only description without running tests
+test(458, examples=True)  # same as above but with examples
+``` 
+"""
 
+
+def build_table():
+    _t = Table(title='[bold]Test Results[/bold]', highlight=True, box=box.MINIMAL)
+    _t.add_column("#:", justify='right')
+    _t.add_column("Expected:", justify='center')
+    _t.add_column("Got:")
+    return _t
+
+
+def test(challenge: int, solution_func: Optional = None, description: bool = False, examples: bool = False):
+    console = Console(theme=Theme({'numbers': 'blue', 'expected': 'green', 'got': 'red', 'final': 'yellow'}))
+
+    # These functions must be protected to ensure no CHEATING happens. #
+    def _fetch_data(url: str) -> Response:
+        _r = requests.get(url)
+        if _r.status_code == 404:
+            raise ChallengeNotFound(f"Challenge {challenge} is not found")
+        return _r
+
+    def _get_tests(challenge: int) -> list[dict]:
+        url = f"https://raw.githubusercontent.com/beginner-codes/challenges/main/weekday/test_cases_{challenge}.json"
+        response = _fetch_data(url)
         return response.json()
+
+    def _get_info(challenge: int, description: bool, examples: bool) -> str:
+        url = f"https://raw.githubusercontent.com/beginner-codes/challenges/main/weekday/challenge_{challenge}.md"
+        result = ''
+        if description or examples:
+            info = _fetch_data(url)
+            try:
+                if description:
+                    result += info.text.split('##')[0]
+                if examples:
+                    result += '# ' + info.text.split('# ')[2].replace('#', '')
+
+            except IndexError as e:
+                result += e
+        return result + '***'
 
     def _run_tests(tests: list[dict], solution_func) -> list[Result]:
         results = []
@@ -51,28 +98,46 @@ def test(challenge: int, solution_func):
 
         return results
 
-    def _show_results(challenge: int, results: list[Result], total_tests: int):
+    def _show_results(challenge: int, results: list[Result], total_tests: int, info: str):
         failures = 0
+        console.print(Markdown(info))
+        results_table = build_table()
+
         for result in results:
             if result.status == StatusEnum.FAILED:
-                print(
-                    f"Test {result.index} failed:  Expected {result.expected}, got {result.got}."
+                results_table.add_row(
+                    f"[numbers]{result.index}[/numbers]",
+                    f"[expected]{result.expected}[/expected]",
+                    f"[got]{result.got}[/got]"
                 )
                 failures += 1
 
             elif result.status == StatusEnum.EXCEPTION:
-                print(f"Test {result.index} failed:  {result.got!r}")
+                results_table.add_row(
+                    f"Test {result.index} failed",
+                    "",
+                    f"{result.got!r}")
                 failures += 1
 
         if failures:
-            print()
+            console.print(results_table)
 
-        print(f"---- Challenge {challenge} Results ----")
-        print(f"{total_tests - failures} passed, {failures} failed")
+        if total_tests:
+            console.print(f"---- Challenge {challenge} Results ----", style='final')
+            console.print(f"[expected]{total_tests - failures}[/expected] passed, [got]{failures}[/got] failed")
 
-        if not failures:
-            print("\n**** Great job!!! ****")
+            if not failures:
+                console.print("\n[expected][bold]**** Great job!!! ****")
 
-    tests = _get_tests(challenge)
-    results = _run_tests(tests, solution_func)
-    _show_results(challenge, results, len(tests))
+    if solution_func:
+        tests = _get_tests(challenge)
+        results = _run_tests(tests, solution_func)
+    else:
+        tests = []
+        results = []
+    if any([solution_func, examples, description]):
+        info = _get_info(challenge, description, examples)
+    else:
+        info = USAGE
+
+    _show_results(challenge, results, len(tests), info)
